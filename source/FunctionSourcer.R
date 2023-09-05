5c27a476865961316d167a548f873de30a4fc729
Janna Willoughby <jwilloughby@auburn.edu>
2023-09-05 12:03:18 -0500

    updated to file structure

---
+++ source/FunctionSourcer.R
@@ -0,0 +1,8 @@
+#Set working directory, import packages, source functions, 
+setwd(paste(directory,"/source/", sep = ''))    # set temp working directory 
+
+#import packages
+#library()
+
+#source functions - example: source(paste(getwd(), "/AgeUp.R", sep = ''))
+
